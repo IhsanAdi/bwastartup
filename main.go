c606a0344da472c3976a5dc56de3135620406316
IhsanAdi <ihsanadi.pratama@yahoo.com>
2021-08-29 10:21:58 +0700

    update get campaign endpoint

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
 	campaignHandler := handler.NewCampaignHandler(campaignService)
 
 	router := gin.Default()
+	router.Static("/images", "./images")  //server, local
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
